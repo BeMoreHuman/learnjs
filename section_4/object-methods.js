e183554e309aca161ec966106bca95d6dbb5d806
alex_klymenko <klymenko.sasha21@gmail.com>
2019-04-24 18:43:40 +0300

    start section_4, objects-more

---
+++ section_4/object-methods.js
@@ -0,0 +1,23 @@
+// Task #1: Создайте калькулятор
+
+// Создайте объект calculator с тремя методами:
+// read() запрашивает prompt два значения и сохраняет их как свойства объекта
+// sum() возвращает сумму этих двух значений
+// mul() возвращает произведение этих двух значений
+var calculator = {
+	// ...ваш код...
+	read: function() {
+		this.firstNumber = Number(prompt('a?', 0));
+		this.secondNumber = Number(prompt('b?', 0));
+	},
+	sum: function() {
+		return String(this.firstNumber + this.secondNumber);
+	},
+	mul: function() {
+		return this.firstNumber * this.secondNumber;
+	},
+};
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
